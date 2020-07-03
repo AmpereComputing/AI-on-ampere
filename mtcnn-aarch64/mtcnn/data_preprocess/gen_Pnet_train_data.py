59025952ee2bfcd7d32ee9848602603b858566aa
yuanzhangzhu <yuanzhangzhu@yeah.net>
2020-07-03 10:41:15 +0800

    adjust size

--- mtcnn-aarch64/mtcnn/data_preprocess/gen_Pnet_train_data.py
+++ mtcnn-aarch64/mtcnn/data_preprocess/gen_Pnet_train_data.py
@@ -83,7 +83,7 @@ for annotation in annotations:
 
         # ignore small faces
         # in case the ground truth boxes of small faces are not accurate
-        if max(w, h) < 40 or x1 < 0 or y1 < 0:
+        if min(w, h) < 40 or x1 < 0 or y1 < 0:
             continue
 
         # generate negative examples that have overlap with gt
